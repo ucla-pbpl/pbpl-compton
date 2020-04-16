502a76dacf585e45372fc28af29467be483debdf
Brian Naranjo <brian.naranjo@gmail.com>
2020-04-15 21:12:43 -0700

    update collimation angle analysis

--- share/baby-cpt/analysis/collimation-angle/run.py
+++ share/baby-cpt/analysis/collimation-angle/run.py
@@ -5,25 +5,71 @@ import toml
 import numpy as np
 from pbpl import compton
 from Geant4.hepunit import *
-import common
 
-def reconf(conf, desc, num_events, theta0):
+# current (kA)  z (mm)  E (MeV)
+# ============  ======  =======
+# 4.5           0       0.023
+# 4.5           5       0.029
+# 4.5           10      0.036
+# 4.5           220     6.926
+# 4.5           225     7.408
+# 4.5           230     7.917
+
+# 9             0       0.089
+# 9             5       0.109
+# 9             10      0.133
+# 9             220     14.375
+# 9             225     15.429
+# 9             230     16.565
+
+# 18            0       0.216
+# 18            5       0.258
+# 18            10      0.307
+# 18            215     22.010
+# 18            220     23.606
+# 18            225     25.321
+# 18            230     27.165
+
+# z0=5mm  and z1=225mm
+
+def reconf(conf, current, theta0, num_events, filename):
     result = copy.deepcopy(conf)
-    result['PrimaryGenerator']['NumEvents'] = num_events
-    result['PrimaryGenerator']['PythonGeneratorArgs'] = [theta0]
-    result['Detectors']['ComptonScint']['File'] = 'out/' + desc + '.h5'
+    result['PrimaryGenerator']['NumEvents'] = int(num_events)
+    if current == '4500':
+        E_lim = [30*keV, 7.4*MeV]
+    elif current == '9000':
+        E_lim = [100*keV, 15.4*MeV]
+    elif current == '18000':
+        E_lim = [250*keV, 25.3*MeV]
+    else:
+        assert(False)
+    result['PrimaryGenerator']['PythonGeneratorArgs'][3] = (
+        '[MeV*{}, MeV*{}]'.format(*np.array(E_lim)/MeV))
+    result['PrimaryGenerator']['PythonGeneratorArgs'][2] = (
+        '[deg*{0}, deg*{0}]'.format(theta0/deg))
+    result['Detectors']['ComptonScint']['File'] = filename
+    result['Fields']['Cpt']['File'] = '../../field/B-field-{}A.h5'.format(
+        current)
     return result
 
 def main():
-    tr = compton.ParallelTaskRunner()
-    conf = toml.load('pwfa.toml')
+    indices = [
+        [['4500', '9000', '18000'], 'current', None],
+        [deg*np.array((0, 2, 4, 8)), 'theta0', 'deg']]
+    num_events_per_run = 1000000
+    min_events_per_thread = 10000
+    max_num_threads = 100
 
-    for theta0 in common.emission_angles:
-        desc = '{:02d}deg'.format(int(round(theta0/deg)))
-        tr.add_task(compton.Task(
-            reconf(conf, desc, common.num_events, theta0),
-            desc, 'pbpl-compton-mc'))
-    tr.run()
+    conf = toml.load('baby-cpt.toml')
+    compton.RunMonteCarlo(
+        indices, conf, reconf, 'out/12mm-2mm.h5',
+        num_events_per_run, min_events_per_thread, max_num_threads)
+
+    # del conf['Geometry']['World']['Collimator']
+    # compton.RunMonteCarlo(
+    #     indices, conf, reconf, 'out/uncollimated.h5',
+    #     num_events_per_run, min_events_per_thread, max_num_threads)
+
 
 if __name__ == '__main__':
     sys.exit(main())
