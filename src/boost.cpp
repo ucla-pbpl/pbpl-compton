72f3942051e0a6adf2b2870cf603a52a8b512bff
Brian Naranjo <brian.naranjo@gmail.com>
2020-11-29 21:28:37 -0800

    Move boost::python exports into their own files

--- src/boost.cpp
+++ src/boost.cpp
@@ -1,42 +1,19 @@
 // -*- mode: c++; c-file-style: "stroustrup"; c-basic-offset: 4 -*-
-#include <Python.h>
-
-#include <cassert>
-#include <iostream>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <map>
-#include <cmath>
-#include <stdexcept>
-#include <memory>
-
-#include <boost/preprocessor/seq/for_each.hpp>
-
-#include <boost/shared_ptr.hpp>
 #include <boost/python.hpp>
-#include <boost/python/call.hpp>
-#include <boost/python/list.hpp>
-//#include <boost/python/numeric.hpp>
-#include <boost/python/extract.hpp>
-#include <boost/python/suite/indexing/vector_indexing_suite.hpp>
-#include <boost/python/suite/indexing/map_indexing_suite.hpp>
-#include <boost/python/enum.hpp>
-#include <boost/python/def.hpp>
-#include <boost/python/module.hpp>
-
+#include <boost/python/numpy.hpp>
 #include "PhysicsList.h"
-#include <G4ParticleGun.hh>
-#include <G4UIExecutive.hh>
 #include "ImportedMagneticField.h"
-#include <CADMesh.hh>
 #include <G4VSolid.hh>
 #include <G4SDManager.hh>
-#include <boost/multi_array.hpp>
-
 #include <G4AssemblyVolume.hh>
 
+#include "Exception.h"
+#include "pyCADMesh.h"
+#include "pyG4MultiSensitiveDetector.h"
+#include "pyG4MaterialPropertiesTable.h"
+
 namespace bp = boost::python;
+namespace np = boost::python::numpy;
 
 template<class T> struct VecToList
 {
@@ -48,83 +25,13 @@ template<class T> struct VecToList
     }
 };
 
-typedef std::map<CADMesh::File::Type, G4String> map_t;
-
-void export_cadmesh()
-{
-    bp::object fileModule(
-        bp::handle<>(bp::borrowed(
-                         PyImport_AddModule("compton.cadmesh"))));
-    bp::scope().attr("cadmesh") = fileModule;
-    bp::scope file_scope = fileModule;
-
-//    G4VSolid* (CADMesh::*bah)(int) = &CADMesh::TessellatedMesh;
-    bp::class_<CADMesh::TessellatedMesh>(
-        "TessellatedMesh", bp::init<char *>())
-        .def("IsValidForNavigation", &CADMesh::TessellatedMesh::IsValidForNavigation)
-        .def("GetFileName", &CADMesh::TessellatedMesh::GetFileName)
-        .def("GetFileType", &CADMesh::TessellatedMesh::GetFileType)
-        .def("GetVerbose", &CADMesh::TessellatedMesh::GetVerbose)
-        .def("SetVerbose", &CADMesh::TessellatedMesh::SetVerbose)
-        .def("GetScale", &CADMesh::TessellatedMesh::GetScale)
-        .def("SetScale", &CADMesh::TessellatedMesh::SetScale)
-        .def("GetOffset", &CADMesh::TessellatedMesh::GetOffset)
-        .def("SetOffset", &CADMesh::TessellatedMesh::SetOffset)
-        .def("GetAssembly", &CADMesh::TessellatedMesh::GetAssembly,
-             bp::return_internal_reference<>())
-        .def<G4VSolid* (CADMesh::TessellatedMesh::*)(G4String)>(
-            "GetSolid", &CADMesh::TessellatedMesh::GetSolid,
-            bp::return_internal_reference<>())
-        .def<G4VSolid* (CADMesh::TessellatedMesh::*)(G4int)>(
-            "GetSolid", &CADMesh::TessellatedMesh::GetSolid,
-            bp::return_internal_reference<>());
-
-
-    // bp::object fileModule(
-    //     bp::handle<>(bp::borrowed(
-    //                      PyImport_AddModule("compton.cadmesh"))));
-    // bp::scope().attr("file") = fileModule;
-    // bp::scope file_scope = fileModule;
-
-    bp::enum_<CADMesh::File::Type>(
-        "Type",
-R"(
-
-
-=========  =========  ==============================
-Attribute  Extension  TypeName
-=========  =========  ==============================
-Unknown    unknown    Unknown File Format
-PLY        ply        Stanford Triangle Format (PLY)
-STL        stl        Stereolithography (STL)
-DAE        dae        COLLADA (DAE)
-OBJ        obj        Wavefront (OBJ)
-TET        tet        TetGet (TET)
-OFF        off        Object File Format (OFF)
-=========  =========  ==============================
-
-)")
-        .value("PLY", CADMesh::File::Type::PLY)
-        .value("STL", CADMesh::File::Type::STL)
-        .value("DAE", CADMesh::File::Type::DAE)
-        .value("OBJ", CADMesh::File::Type::OBJ)
-        .value("TET", CADMesh::File::Type::TET)
-        .value("OFF", CADMesh::File::Type::OFF);
-
-
-    bp::class_<map_t>("TypeMap")
-        .def(bp::map_indexing_suite<map_t>());
-    bp::scope().attr("Extension") = CADMesh::File::Extension;
-    bp::scope().attr("TypeString") = CADMesh::File::TypeString;
-    bp::scope().attr("TypeName") = CADMesh::File::TypeName;
-}
-
-
 BOOST_PYTHON_MODULE(boost)
 {
     Py_Initialize();
 
     bp::docstring_options local_docstring_options(true, false, false);
+
+    np::initialize();
 
     bp::to_python_converter<
 	std::vector<std::string, class std::allocator<std::string> >,
@@ -190,6 +97,7 @@ Args:
   value (float): set scaling factor (initially defaults to 1.0)
 )");
 
-    export_cadmesh();
-//    export_cadmesh_file();
+    export_CADMesh();
+    export_G4MultiSensitiveDetector();
+    export_G4MaterialPropertiesTable();
 }
