add parameter search, correct normalization

share/baby-cpt/analysis/train/data_normalization.py
@@ -7,24 +7,40 @@ def normalize_examples(conf, examples):
     #max_ex = 0.1e-9
     print(examples.shape)
     default_max_ex = conf['NeuralNetwork']["DefaultMaxEx"]
-    print("data_normalization normalize: max_ex", default_max_ex)
+    print("data_normalization normalize: normalize_examples: default_max_ex", default_max_ex)
     normalized = examples/default_max_ex/z_weights*80
-    print("max normalized: ", np.max(normalized))
+    print("data_normalization normalize: normalize_examples: max normalized: ", np.max(normalized))
     center = 0#np.max(normalized)/2
-    print(normalized.shape)
+    print("data_normalization normalize: normalize_examples: normalized.shape", normalized.shape)
     return (normalized-center, default_max_ex)
-
+'''
 def normalize_examples_indi(conf, examples): #n, 64, 128
     count = examples.shape[0]
-    print(count)
+    print("normalize_examples_indi: normalize_examples_indi: count", count)
     examples_flattened = examples.reshape(count, -1)#N, 128*64
     max_ex = np.max(examples_flattened.T, axis = 0) # N
-    print(max_ex)
+    print("normalize_examples_indi: normalize_examples_indi: max_ex", max_ex)
     examples_normalized = (examples_flattened.T)/max_ex #128*64, N
     weighted = examples_normalized.T.reshape(count, -1, 128)/z_weights*80
-    print("max normalized: ", np.max(weighted))
-    print(max_ex)
+    print("normalize_examples_indi: normalize_examples_indi: max of weighted",  np.max(weighted))
+    #print(max_ex)
     return (weighted, max_ex)
+'''
+def normalize_examples_indi(conf, examples): #n, 64, 128
+    count = examples.shape[0]
+    print("data_normalization: normalize_examples_indi: count", count)
+    examples_weighted = examples/z_weights*80
+    examples_flattened = examples_weighted.reshape(count, -1)#N, 128*64
+    max_weighted_ex = np.max(examples_flattened.T, axis = 0) # N
+    print("data_normalization: normalize_examples_indi: max_weighted_ex", max_weighted_ex)
+    default_max_ex = conf['NeuralNetwork']["DefaultMaxEx"]
+    print("data_normalization normalize: normalize_examples: default_max_ex", default_max_ex)
+    target = 1000*8e-8/default_max_ex
+    examples_normalized = (examples_flattened.T)/max_weighted_ex*target #128*64, N
+    weighted = examples_normalized.T.reshape(count, -1, 128)
+    print("data_normalization: normalize_examples_indi: max of weighted",  np.max(weighted))
+    #print(max_ex)
+    return (weighted, max_weighted_ex/target)
     
 def normalize_labels(conf, labels):
      #1.5e7
@@ -32,20 +48,20 @@ def normalize_labels(conf, labels):
     return labels/default_ratio
 
 def recover_labels(conf, predicted):
-    print("data_normalization recover: max_ex", max_ex)
+    print("data_normalization: recover_labels: max_ex", max_ex)
     default_ratio = conf['NeuralNetwork']["DefaultRatio"]
     labels=predicted*default_ratio
-    print("max labels: ", np.max(labels))
+    print("data_normalization: recover_labels: max of labels", np.max(labels))
     return labels
 
 def recover_labels_indi(conf, predicted, max_ex): #N, 128 || N
     default_max_ex = conf['NeuralNetwork']["DefaultMaxEx"]
     default_ratio = conf['NeuralNetwork']["DefaultRatio"]
     print(predicted.shape)
-    print("data_normalization recover: max_ex", max_ex)
+    print("data_normalization: recover_labels_indi: max_ex", max_ex)
     print("ratio ", max_ex/default_max_ex)
     labels=((predicted.T)*default_ratio*max_ex/default_max_ex).T
-    print("max labels: ", np.max(labels))
+    print("data_normalization: recover_labels_indi: max of labels", np.max(labels))
     return labels
     
     

share/baby-cpt/analysis/train/predict_summed_image_darpa.py
@@ -9,29 +9,9 @@ from mpl_toolkits.mplot3d import Axes3D
 import argparse
 import toml
 from skimage.transform import resize
+import scipy.stats as stats
 from pbpl import common
 import data_normalization
-
-
-# (py37) rodan:~/pbpl-compton/share/baby-cpt/analysis/task1/models-selected-Emily-Lisa-Rose-Adam-Bella-THALHEIMER> 
-# python ../../train/predict_summed_image_reusable.py --h5_name pwfa --dir ../fake-data --config ../config/task1-Emily-Lisa-Rose-Adam-Bella-THALHEIMER.toml --out jan29 --model ./long_ht_n_glong_ht_n_3plong_ht_n_7g.ckpt
-
-#(py37) rodan:~/pbpl-compton/share/baby-cpt/analysis/nonstripe4500/models-grid-Emily-Lisa-Rose-Adam-Bella-THABIT>
-# python ../../train/predict_summed_image_reusable.py --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/nonstripe4500-Emily-Lisa-Rose-Adam-Bella-THABIT.toml --out Mar10 --model ./long_ht_n_glong_ht_n_3plong_ht_n_7g.ckpt
-# python ../../train/predict_summed_image_reusable.py --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/nonstripe4500-Emily-Lisa-Rose-Adam-Bella-THABIT.toml --out Mar10-3p2 --model ./long_ht_n_glong_ht_n_3p2long_ht_n_7g.ckpt
-
-# (py37) rodan:~/pbpl-compton/share/baby-cpt/analysis/nonstripe4500/models-grid-THABIT-Copy>
-# python ../../train/predict_summed_image_reusable.py --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/THABIT-Copy.toml --out Mar10 --model ./long_ht_n_glong_ht_n_3p2long_ht_n_7g.ckpt
-
-# python ../../train/predict_summed_image_reusable.py --lower_cutoff 2e-1 --out Mar17_bigger_target --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/THABIT-Copy.toml --model ./long_ht_n_glong_ht_n_3p2long_ht_n_7g.ckpt
-# python ../../train/predict_summed_image_reusable.py --lower_cutoff 2e-1 --out April25_bigger_target --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/THABIT-Copy.toml --model ./long_ht_n_glong_ht_n_3p2long_ht_n_7g.ckpt
-
-# (py37) rodan:~/pbpl-compton/share/baby-cpt/analysis/nonstripe4500/models-grid-THABIT-Copy-inverse>
-# python ../../train/predict_summed_image_reusable.py --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/THABIT-Copy-inverse.toml --out MApril14 --model ./long_inv_3plong_inv_7g.ckpt
-
-# (py37) rodan:~/pbpl-compton/share/baby-cpt/analysis/nonstripe4500/models-grid-THABIT-Copy-walk>
-# python ../../train/predict_summed_image_reusable.py --h5_name pwfa5e10-log-cropped --dir ../fake-data --config ../config/THABIT-Copy-walk.toml --out April25 --model ./walk_3000_1.ckpt --lower_cutoff 2e-1
-# python ../../train/predict_summed_image_reusable.py --h5_name darpa --dir ../fake-data --config ../config/THABIT-Copy-walk.toml --out May2-walk --model ./walk_3000_1.ckpt --lower_cutoff 2e-1
 
 def preprocess_summed_h5(filename, config_file):
     gin = h5py.File(filename, 'r')
@@ -55,14 +35,7 @@ def main(args):
     #filename = 'test-right-y/varY-col-2e6-UPFZET-0.h5'
 
     gin = h5py.File(filename, 'r')
-    truth = np.zeros([50, 31])
     #name = os.path.splitext(filename)[0]
-
-    try:
-        with np.load(dirname+'/'+name+'.npz') as data:
-            truth = data['histo']
-    except (IOError):
-        truth = None
 
 
     edep = gin['compton-electron']['edep'][0] # (5, 300, 450)
@@ -111,42 +84,49 @@ def main(args):
         model.predict([[edep_normalized[0, int(y_bins/2), np.newaxis]]]), edep_max)
 
     common.setup_plot()
-    #fig3 = plt.figure(figsize=(3+3/8, 4+4/8), dpi=600, constrained_layout=True)
-    #gs = fig3.add_gridspec(nrows=2, ncols=1, height_ratios = [10., 7.])
-    fig3 = plt.figure(figsize=(3+3/8, 2+2/8), dpi=600, constrained_layout=True)
-    gs = fig3.add_gridspec(nrows=1, ncols=1)
+    fig3 = plt.figure(figsize=(3+3/8, 4+4/8), dpi=600, constrained_layout=True)
+    gs = fig3.add_gridspec(nrows=2, ncols=1, height_ratios = [10., 7.])
 
-    f3_ax1 = fig3.add_subplot(gs[0, 0])
+    f3_ax1 = fig3.add_subplot(gs[1, 0])
     photon_e_bins = np.logspace(np.log(l_e_lower), np.log(l_e_upper), l_e_bins+1, base=np.e)
     print(photon_e_bins)
-    
+
+    try:
+        with np.load(dirname+'/'+name+'.npz') as data:
+            truth = data['histo']
+    except (IOError):
+        photon_energies = np.array([1, 1.8, 2, 3])
+        photon_number = np.array([1e9, 1e9, 1e9, 1e9])
+        width = 0.001
+        truth = np.zeros((l_y_bins, l_e_bins))
+        for i in range(4):
+            ej_l = photon_energies[i]-3*width
+            ej_u = photon_energies[i]+3*width
+            x = np.linspace(ej_l, ej_u, 100)
+            float_weights = stats.norm.pdf(x, photon_energies[i], width/2)*photon_energies[i]*photon_number[i]
+            #print(float_weights)
+            truth[int(l_y_bins/2)] = truth[int(l_y_bins/2)] + np.interp(photon_e_bins[:-1], x, float_weights)
+            
+
+    if truth is not None:
+        truth_c = truth[int(l_y_bins/2)]
+        #f3_ax1.plot(photon_e_bins[:-1], truth_c, label = "truth")
+        f3_ax1.set_title("truth vs prediction")
+        
+        #f3_ax1_c = fig3.add_subplot(gs[0, 3])
+        #fig3.colorbar(truth_im, cax=f3_ax1_c, shrink=0.6)
 
     prediction = test_predictions.reshape(l_y_bins, l_e_bins)
     prediction_c = prediction[int(l_y_bins/2)]
     f3_ax1.plot(photon_e_bins[:-1], prediction_c.clip(min=0), label = "prediction")
     #pre_im = f3_ax2.imshow(prediction)
     #f3_ax2.set_title("prediction")
-    if truth is not None:
-        truth_c = truth[int(l_y_bins/2)]
-        f3_ax1.plot(photon_e_bins[:-1], truth_c, label = "truth")
-        #f3_ax1.set_title("truth vs prediction")
-        
-        max_truth = np.max(truth_c)
-        truth_c_normalized = truth_c/max_truth
-        prediction_normalized = prediction_c.clip(min=0)/max_truth
-        RMSE = np.sqrt(np.mean((prediction_normalized-truth_c_normalized)**2))
-        print(f"---------------max truth: {max_truth}, RMSE: {RMSE}---------------")
-        #---------------max truth: 1118978413.8537161, RMSE: 0.11553199924011477---------------
 
-    f3_ax1.yaxis.grid(True, which='major')
     f3_ax1.set_xscale('log')
-    f3_ax1.set_xlim(left=args.lower_cutoff)
-    f3_ax1.set_yscale('log')
     f3_ax1.set_xlabel('energy (MeV)')
     f3_ax1.set_ylabel('photon energy density (MeV/MeV)')
     f3_ax1.legend()
-    
-    '''
+
     f3_ax3 = fig3.add_subplot(gs[0, 0])
     f3_ax3.set_title('gs[:, 0]')
     #im = f3_ax3.imshow(edep_normalized, interpolation='bilinear', origin='lower')
@@ -162,7 +142,6 @@ def main(args):
     #fig.subplots_adjust(right=0.8)
     #cbar_ax = fig.add_axes([0.85, 0.15, 0.05, 0.7])
     #fig.colorbar(truth_im, cax=cbar_ax)
-    '''
     model_folder = os.path.basename(os.path.dirname(checkpoint_path))
     plt.savefig(model_folder+'/'+args.out+"-"+name+".png")
     np.savez(model_folder+'/'+args.out+"-"+name+".npz", prediction=prediction)
@@ -170,18 +149,15 @@ def main(args):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description='Predict gamma spectra based on spectrometer data.')
-    parser.add_argument("--h5_name", required=True,
-        help="set where spectrometer output is.")
-    parser.add_argument("--dir", required=True,
-        help="set where the h5 file is in")
     parser.add_argument("--model", required=True,
         help="path to saved tensorflow model")
     parser.add_argument("--config", required=True,
         help="the config toml file")
     parser.add_argument("--out", required=True,
         help="output file name suffix")
-    parser.add_argument("--lower_cutoff", type=float,
-        help="set the lower bound for the plot.")
+
     
     args = parser.parse_args()
+    args.dir = "fake-data"
+    args.h5_name = "darpa"
     main(args)